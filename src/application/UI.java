8e200a9df241310a18a57ef67b642fa03d94f7aa
Vinicius de Jesus <viniciusdejjesuspessoal@gmail.com>
2025-12-01 08:26:37 -0300

    limpando a tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,6 +32,12 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {// limpa a tela ao imprimir o tabuleiro
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
